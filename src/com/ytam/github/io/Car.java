8912f9e1f67a78712ce3a9d6c5f25f9a3839efa0
brusk <tamyldrm@gmail.com>
2016-12-09 20:25:12 +0300

    OOP

---
+++ src/com/ytam/github/io/Car.java
@@ -0,0 +1,34 @@
+package com.ytam.github.io;
+
+/**
+ * Created by brusk on 12/9/16.
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String  model;
+    private String  engine;
+    private String  colour;
+
+
+    public void setModel(String model){
+
+        String validModel =model.toLowerCase();
+
+        if(validModel.equals("Porcshe")){
+            this.model=model;
+
+        }
+        else {
+            this.model = "Unknown";
+
+        }
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+}
---
+++ src/com/ytam/github/io/Main.java
@@ -0,0 +1,19 @@
+package com.ytam.github.io;
+
+/**
+ * Created by brusk on 12/9/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porsche= new Car();
+        porsche.setModel("ddsfsdf");
+
+        System.out.println("Model is :"+ porsche.getModel());
+
+
+
+        Car holden= new Car();
+
+}}
